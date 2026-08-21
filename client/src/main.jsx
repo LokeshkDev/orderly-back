6a5e9448b94ac02ba3a865a690b13ec826eb2542
lokeshk2492 <lokeshk2492@gmail.com>
2026-08-21 16:41:49 +0530

    change of UI bs5

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/theme.css'
 import './index.css'
 
